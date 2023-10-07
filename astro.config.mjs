8a877a780375c89f7279496c67626225558fd333
nash1111 <ken1251213@gmail.com>
2023-10-07 23:43:12 +0900

    feat: add katex settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown: {
+		remarkPlugins: [remarkMath],
+		rehypePlugins: [rehypeKatex],
+	  },
 });
---
+++ src/content/blog/katex-test.md
@@ -0,0 +1,12 @@
+---
+title: 'KaTeX rendering test'
+description: 'katex test'
+pubDate: 'Oct 7 2023'
+heroImage: '/blog-placeholder-2.jpg'
+---
+
+KaTex
+$X_t$
+```
+$X_t$
+```
